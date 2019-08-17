c4796d81449f7e38fb33710ebcf299be204a3f3c
Cecile Tonglet <cecile.tonglet@cecton.com>
2019-08-17 11:04:17 +0200

    Attempt to have more constant benchmarks

--- benches/processing.rs
+++ benches/processing.rs
@@ -10,7 +10,7 @@ fn processing_speed(b: &mut Bencher) {
     b.iter(|| {
         let txt = "Hello World!";
         let font = Font::new(24);
-        let img = ImageBuffer::new(20, 20);
+        let img = ImageBuffer::new(50, 50);
 
         process(img, txt, &font, || {});
     });
